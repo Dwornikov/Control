758683222c742341efef586bc5d975f01583f340
Denis <den.dvornikov.88@inbox.ru>
2023-05-13 23:08:59 +0600

    Сделал цикл

--- ControlW/Program.cs
+++ ControlW/Program.cs
@@ -2,4 +2,10 @@
 Console.Write("Введите элементы массива через запятую: ");
         string[] input = Console.ReadLine()!.Split(',');
         string[] output = new string[input.Length];
-        
+ int j = 0;
+        for (int i = 0; i < input.Length; i++) {
+            if (input[i].Length <= 3) {
+                output[j] = input[i];
+                j++;
+            }
+        }
