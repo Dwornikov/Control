f3e2c2103eedf341ea6e127b8883d3beec987d7b
Denis <den.dvornikov.88@inbox.ru>
2023-05-13 22:51:27 +0600

    Ввел принятие массива через запятую

---
+++ ControlW/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите элементы массива через запятую: ");
+        string[] input = Console.ReadLine()!.Split(',');
+        string[] output = new string[input.Length];
+        
